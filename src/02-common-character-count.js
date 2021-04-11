7bdef05d5a37a37b642079305899bab62fc427dd
gtycbjyth <kovaltru@gmail.com>
2021-04-12 00:06:32 +0300

    feal: function common character count

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -6,11 +6,10 @@
  * @return {Number}
  *
  * @example
- * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
+ * For s1 = "aabcc" and s2 = "adcaa" = 2, the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  return s2.length - [...s1].reduce((acc, item) => acc.replace(new RegExp(item), ''), s2).length;
 }
-
 module.exports = getCommonCharacterCount;
